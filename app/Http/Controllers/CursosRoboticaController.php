0404dfdf3d9637d6b3cae090d25e662df0b38ad1
Sergio <al02850997@tecmilenio.mx>
2023-03-11 18:12:02 -0600

    Iniciamos control de cambios

---
+++ app/Http/Controllers/CursosRoboticaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\cursosRobotica;
+use Illuminate\Http\Request;
+
+class CursosRoboticaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(cursosRobotica $cursosRobotica)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(cursosRobotica $cursosRobotica)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, cursosRobotica $cursosRobotica)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(cursosRobotica $cursosRobotica)
+    {
+        //
+    }
+}
